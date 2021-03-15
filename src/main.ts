b4a61fe1efa067c10a4f23a15dd7e091be61fe22
Simon Malinge <simon.malinge@hiloenergie.com>
2021-03-14 21:30:17 -0400

    add MQTT connectivity

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,39 @@
 import * as fs from 'fs';
+import * as mqtt from 'mqtt';
 import * as os from 'os';
 import * as path from 'path';
 import { Observable } from 'rxjs';
-import { distinctUntilChanged, map, startWith, throttleTime } from 'rxjs/operators';
+import { distinctUntilChanged, map, startWith, switchMap, tap, throttleTime } from 'rxjs/operators';
 
 const LOG_FILE = path.resolve(os.homedir(), 'Library/Application Support/Microsoft/Teams/logs.txt');
+
+const BASE_TOPIC = `simon_in_teams_call`;
+const AVAILABILITY_TOPIC = `${BASE_TOPIC}/status`;
+const STATE_TOPIC = `${BASE_TOPIC}/state`;
+
+const env = (name: string): string => {
+  const value = process.env[name];
+  if (!value) {
+    console.error(`Missing environment variable ${name}`);
+    process.exit(1);
+  }
+  return value;
+};
+
+const MQTT_BROKER = env('MQTT_BROKER');
+const MQTT_CLIENT_ID = env('MQTT_CLIENT_ID');
+const MQTT_USERNAME = env('MQTT_USERNAME');
+const MQTT_PASSWORD = env('MQTT_PASSWORD');
+
+enum Availability {
+  AVAILABLE = 'online',
+  UNAVAILABLE = 'offline',
+}
+
+enum BinarySensorValue {
+  ON = 'ON',
+  OFF = 'OFF',
+}
 
 const watch = (filename: fs.PathLike) =>
   new Observable<void>((observer) => {
@@ -23,7 +52,7 @@ const getLastCallStatus = (filename: fs.PathLike): boolean =>
     .pop()!
     .match(/eventData: s::;m::1;a::1/) != null;
 
-const watchCallStatus = (filename: fs.PathLike) =>
+const watchCallStatus = (filename: fs.PathLike): Observable<boolean> =>
   watch(filename).pipe(
     startWith(undefined),
     throttleTime(1000),
@@ -31,4 +60,46 @@ const watchCallStatus = (filename: fs.PathLike) =>
     distinctUntilChanged(),
   );
 
-watchCallStatus(LOG_FILE).subscribe((inCall) => console.log(`in call: ${inCall}`));
+const mqttClient$ = new Observable<mqtt.MqttClient>((observer) => {
+  const client = mqtt.connect(`mqtt://${MQTT_BROKER}`, {
+    clientId: MQTT_CLIENT_ID,
+    username: MQTT_USERNAME,
+    password: MQTT_PASSWORD,
+    will: {
+      topic: AVAILABILITY_TOPIC,
+      payload: Availability.UNAVAILABLE,
+      qos: 1,
+      retain: true,
+    },
+  });
+
+  client.on('connect', () => {
+    console.log('MQTT connected!');
+    client.publish(AVAILABILITY_TOPIC, Availability.AVAILABLE, {
+      qos: 1,
+      retain: true,
+    });
+  });
+
+  client.on('error', (error) => console.error('MQTT error:', error));
+
+  observer.next(client);
+
+  return () => client.end();
+});
+
+const main$ = mqttClient$.pipe(
+  switchMap((client) =>
+    watchCallStatus(LOG_FILE).pipe(
+      tap((inCall) => {
+        console.log(`in call: ${inCall}`);
+        client.publish(STATE_TOPIC, inCall ? BinarySensorValue.ON : BinarySensorValue.OFF, {
+          qos: 1,
+          retain: true,
+        });
+      }),
+    ),
+  ),
+);
+
+main$.subscribe();
