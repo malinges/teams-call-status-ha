e06cc0e451f2f2c00c9896a2363097aeb76e6ab4
Simon Malinge <simon.malinge@hiloenergie.com>
2021-04-01 13:34:43 -0400

    try to survive to log file rotation

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import * as fs from 'fs';
 import * as mqtt from 'mqtt';
 import * as os from 'os';
 import * as path from 'path';
-import { Observable } from 'rxjs';
-import { distinctUntilChanged, map, startWith, switchMap, tap, throttleTime } from 'rxjs/operators';
+import { Observable, timer } from 'rxjs';
+import { distinctUntilChanged, first, map, repeat, switchMap, tap, throttleTime } from 'rxjs/operators';
 
 const LOG_FILE = path.resolve(os.homedir(), 'Library/Application Support/Microsoft/Teams/logs.txt');
 
@@ -36,25 +36,47 @@ enum BinarySensorValue {
 }
 
 const watch = (filename: fs.PathLike) =>
-  new Observable<void>((observer) => {
-    const watcher = fs.watch(filename);
-    watcher.on('change', () => observer.next());
-    watcher.on('error', (err) => observer.error(err));
-    watcher.on('close', () => observer.complete());
-    return () => watcher.close();
-  });
+  timer(0, 1000).pipe(
+    first(() => {
+      try {
+        return fs.statSync(filename).isFile();
+      } catch (err) {
+        if (err?.code === 'ENOENT') return false;
+        throw err;
+      }
+    }),
+    switchMap(
+      () =>
+        new Observable<void>((observer) => {
+          observer.next();
+
+          const watcher = fs.watch(filename);
+          watcher.on('change', (eventType) => {
+            if (eventType === 'change') {
+              observer.next();
+            } else {
+              watcher.close();
+            }
+          });
+          watcher.on('error', (err) => observer.error(err));
+          watcher.on('close', () => observer.complete());
+
+          return () => watcher.close();
+        }),
+    ),
+    repeat(),
+  );
 
 const getLastCallStatus = (filename: fs.PathLike): boolean =>
   fs
     .readFileSync(filename, { encoding: 'utf8' })
     .split(/(?:\r\n|\r|\n)/)
     .filter((line) => line.match(/eventData: s::;m::1;a::[13]/))
-    .pop()!
-    .match(/eventData: s::;m::1;a::1/) != null;
+    .pop()
+    ?.match(/eventData: s::;m::1;a::1/) != null;
 
 const watchCallStatus = (filename: fs.PathLike): Observable<boolean> =>
   watch(filename).pipe(
-    startWith(undefined),
     throttleTime(1000),
     map(() => getLastCallStatus(filename)),
     distinctUntilChanged(),
